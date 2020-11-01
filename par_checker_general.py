dea7416b3ade56d460d929accaf2ee0db8c61df1
s.urista <surista@g.harvard.edu>
2020-11-01 14:09:26 +0900

    update

--- par_checker_general.py
+++ par_checker_general.py
@@ -6,23 +6,26 @@ Nov 2020
 Module documentation goes here
 """
 
-
 def par_checker(par_string):
     s = []
-    for x in par_string:
-        if x == '(':
-            s.append(x)
+    pares = {'(':')', '{':'}', '[':']'}
+
+    for char in par_string:
+        if char in pares:
+            s.append(char)
         else:
             if len(s) == 0:
                 return False
             else:
-                if s.pop() != '(':
+                if pares[s.pop()] != char:
                     return False
-
     return len(s) == 0
-
 
 print(par_checker("((()))"))  # expected True
 print(par_checker("((()()))"))  # expected True
 print(par_checker("(()"))  # expected False
 print(par_checker(")("))  # expected False
+print(par_checker('{({([][])}())}')) # expected True
+print(par_checker('[{()]')) #expected false
+
+
